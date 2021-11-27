c583446cae68cc6c66eb075aedc15edb8ea72062
Cooper Browning <Cooper.S.Browning-1@ou.edu>
2021-11-27 11:53:53 -0600

    Idk

---
+++ CooperBrowningWebsite/CooperBWebsite/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace CooperBWebsite
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
